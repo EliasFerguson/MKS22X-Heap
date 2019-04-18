b7d9e8473b9760c28e6f28b53d8d68ea66552b77
Elias Ferguson <eferguson00@stuy.edu>
2019-04-18 11:11:56 -0400

    skeleton

---
+++ MyHeap.java
@@ -0,0 +1,17 @@
+public class MyHeap {
+  public static void main(String[] args) {
+
+  }
+  private static void pushDown(int[] data, int size, int index) {
+
+  }
+  private static void pushIp(int[] data, int index) {
+
+  }
+  public static void heapify(int[] data) {
+
+  }
+  public static void heapsort(int[] data) {
+    
+  }
+}
