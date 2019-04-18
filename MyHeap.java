d462dd4c784ab9f3bcf2b8dd9ef0f3f51043d5ce
Elias Ferguson <eferguson00@stuy.edu>
2019-04-18 11:30:16 -0400

    wrote swap()

--- MyHeap.java
+++ MyHeap.java
@@ -3,9 +3,12 @@ public class MyHeap {
 
   }
   private static void pushDown(int[] data, int size, int index) {
+    boolean done = false;
+    while (!done) {
 
+    }
   }
-  private static void pushIp(int[] data, int index) {
+  private static void pushUp(int[] data, int index) {
 
   }
   public static void heapify(int[] data) {
@@ -14,17 +17,22 @@ public class MyHeap {
   public static void heapsort(int[] data) {
 
   }
-  private int[] getChildren(int index, int[] data) {
+  private static int[] getChildren(int index, int[] data, int size) {
     int[] children = new int[2];
     int retIndex = (2 * index) + 2;
-    if (retIndex < data.length) {
+    if (retIndex < size) {
       children[0] = data[retIndex - 1];
       children[1] = data[retIndex];
     }
     return children;
   }
-  private int getParent(int index, int[] data) {
+  private static int getParent(int index, int[] data) {
     int retIndex = (index - 1) / 2;
     return data[retIndex];
+  }
+  private static void swap(int i1, int i2, int[] data) {
+    int temp = data[i1];
+    data[i1] = data[i2];
+    data[i2] = temp;
   }
 }
