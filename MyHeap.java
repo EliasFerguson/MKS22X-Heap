0758c804c6a5bf9d5a2451c1b05d1c9beb4ab084
Elias Ferguson <eferguson00@stuy.edu>
2019-04-18 11:22:42 -0400

    wrote getChildren and getParent for later use

--- MyHeap.java
+++ MyHeap.java
@@ -12,6 +12,19 @@ public class MyHeap {
 
   }
   public static void heapsort(int[] data) {
-    
+
+  }
+  private int[] getChildren(int index, int[] data) {
+    int[] children = new int[2];
+    int retIndex = (2 * index) + 2;
+    if (retIndex < data.length) {
+      children[0] = data[retIndex - 1];
+      children[1] = data[retIndex];
+    }
+    return children;
+  }
+  private int getParent(int index, int[] data) {
+    int retIndex = (index - 1) / 2;
+    return data[retIndex];
   }
 }
