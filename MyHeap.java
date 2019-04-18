735d5dcc7a2e0c39c93f0404f9ac317c39c50c01
Elias Ferguson <eferguson00@stuy.edu>
2019-04-18 11:39:44 -0400

    progressing through pushDown()

--- MyHeap.java
+++ MyHeap.java
@@ -1,11 +1,22 @@
+import java.util.*;
+import java.io.*;
 public class MyHeap {
   public static void main(String[] args) {
 
   }
   private static void pushDown(int[] data, int size, int index) {
     boolean done = false;
+    int[] kids;
     while (!done) {
-
+      if (children(index, data, size)) {
+        kids = getChildren(index, data);
+        if (kids[0] >= kids[1] && kids[0] > data[index]) {
+          swap(index,  ((index * 2) + 1),data);
+        }
+        else if (kids[1] > kids[0] && kids[1] > data[index]) {
+          swap(index,  ((index * 2) + 2),data);
+        }
+      }
     }
   }
   private static void pushUp(int[] data, int index) {
@@ -17,13 +28,16 @@ public class MyHeap {
   public static void heapsort(int[] data) {
 
   }
-  private static int[] getChildren(int index, int[] data, int size) {
+  private static boolean children(int index, int[] data, int size) {
+    int test = (2 * index) + 2;
+    if (test < size) return true;
+    return false;
+  }
+  private static int[] getChildren(int index, int[] data) {
     int[] children = new int[2];
     int retIndex = (2 * index) + 2;
-    if (retIndex < size) {
-      children[0] = data[retIndex - 1];
-      children[1] = data[retIndex];
-    }
+    children[0] = data[retIndex - 1];
+    children[1] = data[retIndex];
     return children;
   }
   private static int getParent(int index, int[] data) {
