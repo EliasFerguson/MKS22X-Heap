99515fc68831aca11c2121efd6af9c3b230a0fec
Elias Ferguson <eferguson00@stuy.edu>
2019-05-02 07:43:07 -0400

    figured out the problem

--- MyHeap.java
+++ MyHeap.java
@@ -2,7 +2,39 @@ import java.util.*;
 import java.io.*;
 public class MyHeap {
   public static void main(String[] args) {
-    int[] data = {2, 12, 23, 5, 2, 1, 6, 7, 100, 1};
+    /*System.out.println("Size\t\tMax Value\theap/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long htime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++) {
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          MyHeap.heapsort(data2);
+          t2 = System.currentTimeMillis();
+          htime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*htime/btime);
+      }
+      System.out.println();
+    }*/
+    int[] data = {-12, 100, 23142, -2, 4, 1213, -4, 0, 21, 4, 51, -8};
     System.out.println(Arrays.toString(data));
     heapsort(data);
     System.out.println(Arrays.toString(data));
@@ -48,17 +80,18 @@ public class MyHeap {
     }
   }
   public static void heapify(int[] data) {
-   for (int i = data.length - 1; i >= 0; i--) {
+   for (int i = data.length - 1; i > -1; i--) {
      pushDown(data, data.length, i);
     }
   }
   public static void heapsort(int[] data) {
     heapify(data);
+    System.out.println(HeapPrinter.toString(data));
     int size = data.length - 1;
     while (size > 0) {
       swap(0, size, data);
-      size--;
       pushDown(data, size, 0);
+      size--;
     }
   }
   private static boolean hasChildL(int size, int idx) {
